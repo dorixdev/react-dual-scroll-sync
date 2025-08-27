6766a2cf18a209fd73e312d29a604eb7df5169ce
J Alfonso Martinez <mtz.alfonso96@gmail.com>
2025-08-27 12:34:35 -0600

    feat: add Storybook manager configuration and update preview parameters

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,9 +2,8 @@ import type { Preview } from '@storybook/react-vite';
 
 const preview: Preview = {
 	parameters: {
-		chromatic: { pauseAnimationAtEnd: true, delay: 400 },
-		actions: { argTypesRegex: '^on[A-Z].*' },
-		controls: { matchers: { color: /(background|color)$/i, date: /Date$/i } }
+		layout: 'centered',
+		viewport: { disable: true }
 	},
 	tags: ['autodocs']
 };
