94c4bbffa8289ac5bb390c8aaaf685d23400c5d4
J Alfonso Martinez <mtz.alfonso96@gmail.com>
2025-09-04 19:39:09 -0600

    feat(styles): add support for custom className and style in DualScrollSyncContent component

--- lib/components/DualScrollSync/DualScrollSyncContent/DualScrollSyncContent.test.tsx
+++ lib/components/DualScrollSync/DualScrollSyncContent/DualScrollSyncContent.test.tsx
@@ -17,4 +17,17 @@ describe('DualScrollSyncContent', () => {
 
 		expect(getByTestId('test-content-id')).toBeInTheDocument();
 	});
+
+	it('should apply custom className and style', () => {
+		const { getByTestId } = render(
+			<DualScrollSyncContent className="custom-class" style={{ borderWidth: '1px' }}>
+				<div>Styled Content</div>
+			</DualScrollSyncContent>
+		);
+
+		const content = getByTestId('test-content-id');
+
+		expect(content).toHaveClass('custom-class');
+		expect(content).toHaveStyle('border-width: 1px');
+	});
 });
